6934cdd3ddf200cece7ff0e4bfbfea7688fbf923
elenamartincantero <72609858+elenamartincantero@users.noreply.github.com>
2021-04-07 12:16:59 +0200

    Update Song.vb

--- Songify/Domain/Song.vb
+++ Songify/Domain/Song.vb
@@ -3,8 +3,10 @@
     Public Property sName As String
     Public Property length As Double
     Public Property playbacks As Collection
+    Public ReadOnly Property SongDAO As SongDAO
 
     Public Sub New(sName As String)
+        Me.SongDAO = new SongDAO 
         Me.sName = sName
     End Sub
 
