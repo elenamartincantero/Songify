b111419d2df065357514888cfdaa09a2c494f8ce
ELENA MARTÍN CANTERO <Elena.Martin19@alu.uclm.es>
2021-04-10 20:07:33 +0200

    Metodo convertLength implementado

--- Songify/Persistence/SongDAO.vb
+++ Songify/Persistence/SongDAO.vb
@@ -1,5 +1,8 @@
 ﻿Public Class SongDAO
     Private Property sortedSongs As Collection
+    Public Sub New()
+        sortedSongs = New Collection
+    End Sub
     Public Sub read(ByRef s As Song)
 
     End Sub
@@ -13,10 +16,16 @@
 
     End Sub
     Public Function convertLength(s As Song) As String
-        Return "tengo que hacer el código"
+        Dim seg As Integer
+        Dim min As Double
+        seg = s.length Mod 60
+        min = (s.length - seg) / 60
+
+
+        Return min.ToString() + ":" + seg.ToString()
     End Function
     Public Sub play(us As User, s As Song)
-
+        'Insert playback
     End Sub
     Public Function readPlayblacks(s As Song) As String
         Return "tengo que hacer el código"
