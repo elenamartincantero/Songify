160b43e3f1fe3fbac8a6c284975c4f63ab981967
Alonsovmoreno <82242401+Alonsovmoreno@users.noreply.github.com>
2021-04-15 00:42:18 +0200

    Update frmSearch.vb

--- Songify/Presentation/frmSearch.vb
+++ Songify/Presentation/frmSearch.vb
@@ -111,6 +111,7 @@ Public Class frmSearch
         HistoryLabel.Visible = True
     End Sub
 
+
     Private Sub PlayButton_Click(sender As Object, e As EventArgs) Handles PlayButton.Click
         Dim song As Song = New Song(Me.SongList.SelectedItem.ToString)
         song.play(Me.user)
