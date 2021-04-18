f7f71a01755195ed4bce7e0dcc51b479f2cf2a6c
ELENA MARTÍN CANTERO <Elena.Martin19@alu.uclm.es>
2021-04-18 20:06:05 +0200

    Cambios en sentencias sql

--- Songify/Persistence/UserDAO.vb
+++ Songify/Persistence/UserDAO.vb
@@ -39,99 +39,44 @@
     End Sub
     Public Sub readMyArtists(ByRef u As User)
         Dim col1 As Collection : Dim aux1 As Collection
-        Dim col2 As Collection : Dim aux2 As Collection
         Dim artist As Artist
-        col1 = DBBroker.GetBroker().Read("SELECT artist FROM FAV_ARTISTS WHERE user='" & u.email & "';")
+        col1 = DBBroker.GetBroker().Read("SELECT aName FROM ARTISTS WHERE IdArtist In (Select artist FROM FAV_ARTISTS WHERE user='" & u.email & "');")
         If col1.Count = 0 Then
             Throw New Exception()
         End If
         For Each aux1 In col1
-            col2 = DBBroker.GetBroker().Read("SELECT aName FROM ARTISTS WHERE IdArtist=" & aux1(1).ToString & ";")
-            For Each aux2 In col2
-                artist = New Artist(aux2(1).ToString)
-                u.fav_artists.Add(artist, artist.name)
-            Next
+            artist = New Artist(aux1(1).ToString)
+            u.fav_artists.Add(artist, artist.name)
         Next
     End Sub
     Public Sub usersSortedByTime()
-        Dim col1 As Collection : Dim aux1 As Collection
-        Dim col2 As Collection : Dim aux2 As Collection
-        Dim song As Song = Nothing
-        Dim user As User = Nothing
-        Dim i As Integer = 0, total = 0
-        col1 = DBBroker.GetBroker().Read("SELECT user, song FROM PLAYBACKS;")
-        If col1.Count = 0 Then
+        Dim col As Collection : Dim aux As Collection
+        Dim user As User
+        col = DBBroker.GetBroker().Read("select p.user, sum(s.length) From playbacks p, songs s where p.song = s.IdSong group by p.user order by sum(s.length);")
+        If col.Count = 0 Then
             Throw New Exception()
         End If
-        For Each aux1 In col1
-            i += 1
-            If i = 1 Then
-                user = New User(aux1(1).ToString)
-            End If
-            col2 = DBBroker.GetBroker().Read("SELECT sName FROM SONGS WHERE IdSong=" & aux1(2).ToString & ";")
-            For Each aux2 In col2
-                song = New Song(aux2(1).ToString)
-                song.readSong()
-                If user.email.Equals(aux1(1).ToString) Then
-                    total += song.length
-                Else
-                    If total = 0 Then
-                        Me.usersByTime.Add(user, song.length.ToString)
-                    Else
-                        Me.usersByTime.Add(user, total.ToString)
-                    End If
-                    total = 0
-                    user = New User(aux1(1).ToString)
-                End If
-            Next
+        For Each aux In col
+            user = New User(aux(1).ToString)
+            usersByTime.Add(user, aux(2).ToString)
         Next
-        If usersByTime.Count < col1.Count Then
-            Me.usersByTime.Add(user, song.length.ToString)
-        End If
-        'FALTA ORDENARLOS
-
-
     End Sub
 
     Public Sub readArtistsMostListened(beginDate As Date, endDate As Date, user As User)
 
     End Sub
     Public Function playbackFavArtists(u As User) As String
-        Dim col1 As Collection : Dim aux1 As Collection
-        Dim col2 As Collection : Dim aux2 As Collection
-        Dim song As Song = Nothing
+        Dim col As Collection : Dim aux As Collection
         Dim txt As String = ""
-        Dim artist As Artist = Nothing
         Dim i As Integer = 0, total = 0
-        col1 = DBBroker.GetBroker().Read("SELECT song FROM PLAYBACKS WHERE user='" & u.email & "';")
-        If col1.Count = 0 Then
+        col = DBBroker.GetBroker().Read("select a.aName, sum(s.length) from artists a, songs s where (a.IdArtist in (select artist from fav_artists where user like '" & u.email & "')) and s.album in (select IdAlbum from Albums where artist = a.IdArtist) and s.IdSong in (select song from playbacks where user like '" & u.email & "') group by a.aName;")
+        If col.Count = 0 Then
             Throw New Exception()
         End If
-        For Each aux1 In col1
-            i += 1
-            col2 = DBBroker.GetBroker().Read("SELECT sName FROM SONGS WHERE IdSong=" & aux1(1).ToString & ";")
-            For Each aux2 In col2
-                song = New Song(aux2(1).ToString)
-                song.readSong()
-                song.album.readAlbum()
-                If i = 1 Then
-                    artist = song.album.artist
-                End If
-
-                If u.fav_artists.Contains(song.album.artist.name) Then
-                    If song.album.artist.Equals(artist) Then
-                        total += song.length
-                    Else
-                        txt += song.album.artist.name + ": " + total.ToString + "s"
-                        total = 0
-                        artist = song.album.artist
-                    End If
-                End If
-
-            Next
-
+        For Each aux In col
+            txt += aux(1).ToString + ": " + aux(2).ToString + "s\n"
         Next
-        txt += song.album.artist.name + ": " + total.ToString + "s"
+
         Return txt
     End Function
 End Class
