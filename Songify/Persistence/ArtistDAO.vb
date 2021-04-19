7a72e5e6366cb120bbf5f510a2eb55c5da533e6b
ELENA MARTÍN CANTERO <Elena.Martin19@alu.uclm.es>
2021-04-19 17:48:07 +0200

    update

--- Songify/Persistence/ArtistDAO.vb
+++ Songify/Persistence/ArtistDAO.vb
@@ -32,7 +32,7 @@
         DBBroker.GetBroker.Change("INSERT INTO ARTISTS VALUES ('" & a.name & "', '" & a.country & "','" & a.image & "');")
     End Sub
     Public Sub update(a As Artist)
-        DBBroker.GetBroker.Change("UPDATE ARTISTS SET aName='" & a.name & "',country='" & a.country & "',image=" & a.image & " WHERE IdArtist='" & a.id & "';")
+        DBBroker.GetBroker.Change("UPDATE ARTISTS SET aName='" & a.name & "',country='" & a.country & "',image='" & a.image & "' WHERE IdArtist=" & a.id & ";")
     End Sub
     Public Sub delete(a As Artist)
         DBBroker.GetBroker.Change("DELETE FROM ARTISTS WHERE aName='" & a.name & "';")
