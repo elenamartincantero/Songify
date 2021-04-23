e096864b2e8e51428171185ab255322cedabb819
ELENA MARTÍN CANTERO <Elena.Martin19@alu.uclm.es>
2021-04-23 05:33:23 +0200

    Todo hecho

--- Songify/Presentation/frmMainMenu.Designer.vb
+++ Songify/Presentation/frmMainMenu.Designer.vb
@@ -35,7 +35,7 @@ Partial Class frmMainMenu
         '
         Me.ManageButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ManageButton.Location = New System.Drawing.Point(52, 187)
-        Me.ManageButton.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.ManageButton.Margin = New System.Windows.Forms.Padding(2)
         Me.ManageButton.Name = "ManageButton"
         Me.ManageButton.Size = New System.Drawing.Size(118, 41)
         Me.ManageButton.TabIndex = 0
@@ -46,7 +46,7 @@ Partial Class frmMainMenu
         '
         Me.SearchButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.SearchButton.Location = New System.Drawing.Point(232, 187)
-        Me.SearchButton.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.SearchButton.Margin = New System.Windows.Forms.Padding(2)
         Me.SearchButton.Name = "SearchButton"
         Me.SearchButton.Size = New System.Drawing.Size(118, 41)
         Me.SearchButton.TabIndex = 1
@@ -57,7 +57,7 @@ Partial Class frmMainMenu
         '
         Me.StatisticsButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.969231!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.StatisticsButton.Location = New System.Drawing.Point(407, 187)
-        Me.StatisticsButton.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.StatisticsButton.Margin = New System.Windows.Forms.Padding(2)
         Me.StatisticsButton.Name = "StatisticsButton"
         Me.StatisticsButton.Size = New System.Drawing.Size(118, 41)
         Me.StatisticsButton.TabIndex = 2
@@ -67,8 +67,8 @@ Partial Class frmMainMenu
         'ChangeUserButton
         '
         Me.ChangeUserButton.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ChangeUserButton.Location = New System.Drawing.Point(424, 246)
-        Me.ChangeUserButton.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.ChangeUserButton.Location = New System.Drawing.Point(472, 11)
+        Me.ChangeUserButton.Margin = New System.Windows.Forms.Padding(2)
         Me.ChangeUserButton.Name = "ChangeUserButton"
         Me.ChangeUserButton.Size = New System.Drawing.Size(87, 28)
         Me.ChangeUserButton.TabIndex = 3
@@ -79,7 +79,7 @@ Partial Class frmMainMenu
         '
         Me.PictureBox1.Image = Global.Songify.My.Resources.Resources.songify
         Me.PictureBox1.Location = New System.Drawing.Point(52, 11)
-        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(2)
         Me.PictureBox1.Name = "PictureBox1"
         Me.PictureBox1.Size = New System.Drawing.Size(471, 171)
         Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
@@ -102,13 +102,13 @@ Partial Class frmMainMenu
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(570, 282)
+        Me.Controls.Add(Me.ChangeUserButton)
         Me.Controls.Add(Me.LoggedUserLbl)
         Me.Controls.Add(Me.PictureBox1)
-        Me.Controls.Add(Me.ChangeUserButton)
         Me.Controls.Add(Me.StatisticsButton)
         Me.Controls.Add(Me.SearchButton)
         Me.Controls.Add(Me.ManageButton)
-        Me.Margin = New System.Windows.Forms.Padding(2, 2, 2, 2)
+        Me.Margin = New System.Windows.Forms.Padding(2)
         Me.Name = "frmMainMenu"
         Me.Text = "Songify - Main menu"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
